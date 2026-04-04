7f197cc55ba975317dbbd564650e13c24e662661
dhyaneshd25 <dhyaneshdharmikdd@gmail.com>
2026-04-04 17:26:51 +0530

    To-Do index.html added

---
+++ todo-app/src/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/";
+
+  try {
+    await mongoose.connect(uri);
+    console.log(`MongoDB connected: ${mongoose.connection.host}`);
+  } catch (err) {
+    console.error(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.warn("MongoDB disconnected");
+});
+
+module.exports = connectDB;
